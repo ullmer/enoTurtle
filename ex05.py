d3a3e52e7c1ae44e01ef227e5791e0e77a5dc5d8
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-12-28 10:53:45 -0500

    updated library name

--- ex05.py
+++ ex05.py
@@ -7,26 +7,26 @@
 # https://pythonguides.com/python-turtle-draw-letters/
 # https://docs.python.org/3/library/turtle.html
 
-import turtleXi as tx #note that turtle.setup must occur first!
+import enoTurtle as et #note that turtle.setup must occur first!
 
 square = "f50 r90 "  * 4 # describe a square
 star   = "f75 r144 " * 5 # describe a star
 
-tx.followPattern(square)          #draw a square
-tx.followPattern("l90 u f50 d")   #shift up
+et.followPattern(square)          #draw a square
+et.followPattern("l90 u f50 d")   #shift up
 
-tx.followPattern(star)            #draw a star
-tx.followPattern("r54 u f70 d")   #shift up
+et.followPattern(star)            #draw a star
+et.followPattern("r54 u f70 d")   #shift up
 
-tx.followPattern("P - r180 C u f130 l100 f130 d r205 V")
-tx.followPattern("u r90 f20 r90 f140 r220 d A")
+et.followPattern("P - r180 C u f130 l100 f130 d r205 V")
+et.followPattern("u r90 f20 r90 f140 r220 d A")
 
-tx.speed(1000)
-tx.pensize(1)
-tx.color("purple")
+et.speed(1000)
+et.pensize(1)
+et.color("purple")
 for i in range(700):
-  tx.followPattern('f%i l91' % i)
+  et.followPattern('f%i l91' % i)
 
-tx.exitonclick()
+et.exitonclick()
 
 ### end ###
